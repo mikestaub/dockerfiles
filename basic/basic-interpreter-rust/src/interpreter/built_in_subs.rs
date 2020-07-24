ba6c288de47f1cea43948a1d1a3f02a35d4aba2d
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2020-07-24 09:17:07 +0200

    suppress warnings

--- basic/basic-interpreter-rust/src/interpreter/built_in_subs.rs
+++ basic/basic-interpreter-rust/src/interpreter/built_in_subs.rs
@@ -55,7 +55,7 @@ impl<S: Stdlib> Interpreter<S> {
                 let file_access: FileAccess = self.pop_integer().into();
                 let file_handle = self.pop_file_handle();
                 self.file_manager
-                    .open(file_handle, file_name.as_ref(), file_mode)
+                    .open(file_handle, file_name.as_ref(), file_mode, file_access)
                     .map_err(|e| {
                         InterpreterError::new_with_pos(
                             format!("Could not open {}: {}", file_name, e),
